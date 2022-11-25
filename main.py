6c53292a47034a3999124c73fa3deb8a3efecaee
Lucas Anderson Ladislau Aguiar <laanderson183@gmail.com>
2022-11-25 12:36:18 -0400

    primeiro commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route("/funcionario")
+def funcionario():
+    return render_template('funcionarios.html')
+
+@app.route("/projeto")
+def projeto():
+    return render_template('funcionarios.html')
+
+@app.route("/licitacao")
+def licitacao():
+    return render_template('funcionarios.html')
+
+@app.route("/veiculos")
+def veiculos():
+    return render_template('funcionarios.html')
+
+@app.route("/cooperados")
+def cooperados():
+    return render_template('cooperados.html')
+
+
