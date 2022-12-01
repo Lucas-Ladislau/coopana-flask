5ffe94edd3dc6d5a13944e738210faa29dc3c2e9
Kaio Guilherme <kaioguilherme444@gmail.com>
2022-12-01 17:57:29 -0400

    população sql

---
+++ Coopana_populate.sql
@@ -0,0 +1,300 @@
+-- projeto
+
+INSERT INTO  projeto (nome,sigla)
+VALUES
+  ('Quisque Libero LLP','QLL'),
+  ('Pede Cras PC','PCP'),
+  ('Tortor Associates','TA'),
+  ('Ullamcorper Inc.','UI'),
+  ('Mauris Suspendisse Aliquet Associates','MSAA'),
+  ('Sit Amet Corp.','SAC'),
+  ('Volutpat Nulla Facilisis LLC','VNF'),
+  ('Nunc Quisque Ornare Ltd','NQOL'),
+  ('Ornare Egestas Ligula Corp.','OELC'),
+  ('Eget Laoreet Posuere Institute','ELPI'),
+  ('Mauris Molestie Ltd','MML');
+
+--licitacao
+
+INSERT INTO  licitacao (projeto,licitacao_destino	)
+VALUES
+  (1,'licitação exemplo 1'),
+  (2,'licitação exemplo 2'),
+  (3,'licitação exemplo 3'),
+  (4,'licitação exemplo 4'),
+  (5,'licitação exemplo 5'),
+  (6,'licitação exemplo 6'),
+  (7,'licitação exemplo 7'),
+  (8,'licitação exemplo 8'),
+  (9,'licitação exemplo 9'),
+  (10,'licitação exemplo 10'),
+  (11,'licitação exemplo 11');
+
+
+--endereco
+
+INSERT INTO endereco (estado,cidade,rua)
+VALUES
+  ('Paraíba','João Pessoa','6851 Nunc, Avenue'),
+  ('Rio Grande do Sul','Rio Grande','390-7006 Fringilla. St.'),
+  ('Maranhão','Balsas','547-292 Ipsum Road'),
+  ('Bahia','Salvador','789-9030 Nullam Avenue'),
+  ('Paraíba','Sousa','5172 Urna Rd.'),
+  ('Santa Catarina','Florianópolis','564-2855 Fames Street'),
+  ('Paraná','Guarapuava','Ap #433-9817 Ac Street'),
+  ('Goiás','Goiânia','3141 Morbi Road'),
+  ('Rio Grande do Sul','Novo Hamburgo','P.O. Box 575, 2300 Cum Rd.'),
+  ('Pernambuco','Caruaru','4957 Volutpat. Ave'),
+  ('Rio de Janeiro','Duque de Caxias','894-9881 Ac Av.'),
+  ('Minas Gerais','Ipatinga','246-7861 Ac Avenue'),
+  ('Pará','Marabá','Ap #968-9440 Adipiscing St.'),
+  ('Pernambuco','Camaragibe','P.O. Box 445, 9059 Ornare Street'),
+  ('São Paulo','Carapicuíba','Ap #747-526 Lobortis Avenue'),
+  ('São Paulo','Campinas','114-9598 Tellus. St.'),
+  ('Paraíba','Sousa','Ap #873-7331 Magna Street'),
+  ('Ceará','Caucaia','2406 Ac Avenue'),
+  ('Minas Gerais','Contagem','109-7703 Non, Rd.'),
+  ('Paraná','Paranaguá','969-8855 Ridiculus Ave'),
+  ('Rio de Janeiro','São Gonçalo','789-504 Non St.'),
+  ('Pernambuco','Cabo de Santo Agostinho','1840 Penatibus St.'),
+  ('São Paulo','Guarulhos','Ap #608-5552 Sit Rd.'),
+  ('Pernambuco','Petrolina','5884 Non, St.'),
+  ('Maranhão','Paço do Lumiar','453-9528 Ut St.'),
+  ('Ceará','Maranguape','169-5079 Sapien. St.'),
+  ('Ceará','Crato','548-6237 Sem Rd.'),
+  ('São Paulo','Carapicuíba','105-2804 Habitant Av.'),
+  ('Pará','Ananindeua','P.O. Box 924, 8892 Facilisi. Street'),
+  ('Bahia','Lauro de Freitas','893-6564 Lacus. St.');
+INSERT INTO endereco (estado,cidade,rua)
+VALUES
+  ('Minas Gerais','Uberaba','P.O. Box 751, 1677 Eget Ave'),
+  ('Goiás','Valparaíso de Goiás','8242 Massa Avenue'),
+  ('Pernambuco','Petrolina','Ap #458-3722 Turpis Ave'),
+  ('Maranhão','Santa Inês','P.O. Box 291, 9894 Torquent Road'),
+  ('São Paulo','Mauá','P.O. Box 666, 628 Gravida St.'),
+  ('Santa Catarina','Criciúma','Ap #636-6202 Libero. Road'),
+  ('Santa Catarina','Criciúma','P.O. Box 722, 2316 Pharetra Av.'),
+  ('Pernambuco','Camaragibe','6510 Sit Rd.'),
+  ('Paraíba','Sousa','Ap #784-7980 Arcu. Street'),
+  ('Rio de Janeiro','Nova Iguaçu','127-6563 Cursus, Av.'),
+  ('Santa Catarina','Florianópolis','6346 In Street'),
+  ('Minas Gerais','Uberaba','Ap #363-4923 Consectetuer Av.'),
+  ('Ceará','Sobral','365-2592 Magnis Road'),
+  ('Goiás','Anápolis','P.O. Box 253, 4575 In St.'),
+  ('São Paulo','Carapicuíba','P.O. Box 916, 8666 Ullamcorper, Road'),
+  ('São Paulo','Piracicaba','P.O. Box 554, 1804 Felis Street'),
+  ('Pará','Parauapebas','Ap #574-6745 Elementum Av.'),
+  ('Ceará','Sobral','P.O. Box 770, 8189 Nisl. Ave'),
+  ('Ceará','Juazeiro do Norte','P.O. Box 846, 1718 Ante St.'),
+  ('São Paulo','Diadema','Ap #428-2647 Adipiscing St.');
+INSERT INTO endereco (estado,cidade,rua)
+VALUES
+  ('Minas Gerais','Uberaba','Ap #268-5064 Diam. Street'),
+  ('Paraná','Foz do Iguaçu','Ap #507-7838 Libero. Street'),
+  ('Minas Gerais','Uberlândia','Ap #687-5792 Ullamcorper. St.'),
+  ('Ceará','Caucaia','990-1628 Vel, St.'),
+  ('Goiás','Anápolis','538-5149 Magnis St.'),
+  ('Santa Catarina','Blumenau','P.O. Box 588, 5155 Litora Rd.'),
+  ('Paraíba','Patos','1537 Cras Av.'),
+  ('Ceará','Maranguape','Ap #985-4895 Scelerisque Rd.'),
+  ('Rio de Janeiro','Niterói','Ap #359-8866 Luctus St.'),
+  ('Paraná','Paranaguá','8968 Metus Avenue'),
+  ('Rio Grande do Sul','Pelotas','Ap #527-4895 Mauris Av.'),
+  ('Rio de Janeiro','São João de Meriti','P.O. Box 533, 6423 Sollicitudin Av.'),
+  ('Paraíba','Sousa','4862 In Ave'),
+  ('Pará','Ananindeua','589-3970 Elementum Av.'),
+  ('Pará','Castanhal','9486 Arcu Av.'),
+  ('Rio de Janeiro','Duque de Caxias','6277 Sit St.'),
+  ('Pará','Castanhal','Ap #312-1675 Eu, Rd.'),
+  ('Bahia','Camaçari','857-5542 Sed Av.'),
+  ('Rio de Janeiro','Niterói','9433 Curae St.'),
+  ('Ceará','Itapipoca','536-2012 Proin Road'),
+  ('Goiás','Aparecida de Goiânia','4829 Vestibulum Road'),
+  ('Paraíba','Bayeux','564-5772 Ut Rd.'),
+  ('Pará','Castanhal','914-322 Congue. Rd.'),
+  ('Ceará','Caucaia','6889 Odio Av.'),
+  ('São Paulo','Campinas','5711 Urna Rd.'),
+  ('Ceará','Caucaia','P.O. Box 614, 8168 A, Av.'),
+  ('Rio Grande do Sul','Gravataí','794-4833 Curae St.'),
+  ('Bahia','Juazeiro','Ap #173-253 Libero. Av.'),
+  ('Ceará','Caucaia','863-9886 In Street'),
+  ('Goiás','Valparaíso de Goiás','876-4188 Non, Rd.');
+INSERT INTO endereco (estado,cidade,rua)
+VALUES
+  ('Ceará','Itapipoca','581-170 Ornare, Rd.'),
+  ('Maranhão','Imperatriz','520-1774 Blandit. Rd.'),
+  ('Pará','Marabá','P.O. Box 282, 8020 Malesuada Rd.'),
+  ('Maranhão','Santa Inês','289-3074 Eu Ave'),
+  ('Pernambuco','Camaragibe','P.O. Box 117, 3210 Sit Road'),
+  ('Bahia','Ilhéus','Ap #449-1762 Ut Rd.'),
+  ('Rio de Janeiro','Niterói','149-3027 Mollis Road'),
+  ('Pará','Cametá','P.O. Box 710, 4824 Nibh Street'),
+  ('Rio Grande do Sul','Porto Alegre','408-9215 A, Road'),
+  ('Santa Catarina','Chapecó','Ap #541-4306 Cursus. Ave'),
+  ('Maranhão','Barra do Corda','P.O. Box 272, 7570 Lorem, Avenue'),
+  ('Minas Gerais','Uberaba','Ap #854-2304 Cursus St.'),
+  ('Pernambuco','Recife','3885 Morbi Rd.'),
+  ('Rio Grande do Sul','Gravataí','P.O. Box 658, 1252 Magna. Rd.'),
+  ('Rio Grande do Sul','Porto Alegre','513-887 Auctor. Road'),
+  ('Paraná','Maringá','Ap #141-2245 Est. St.'),
+  ('Bahia','Lauro de Freitas','Ap #593-3521 Ornare Ave'),
+  ('Rio Grande do Sul','Porto Alegre','P.O. Box 442, 5692 Tempor Rd.'),
+  ('São Paulo','Piracicaba','P.O. Box 253, 5512 Odio Ave'),
+  ('Paraíba','João Pessoa','Ap #939-5862 Enim. St.');
+
+-- Funcionários
+
+INSERT INTO  funcionarios (nome,cpf,telefone,endereco,sexo,cargo)
+VALUES
+  ('Ira Ramos','16.978.639-9','(61) 1684-8779',1,'M','Motorista'),
+  ('Chaim Bryan','45.685.653-5','(14) 3351-3721',2,'M','Motorista'),
+  ('Amal Dale','12.356.661-0','(53) 9197-6692',3,'M','Motorista'),
+  ('Christopher Mcmillan','15.100.754-6','(24) 5314-7858',4,'M','Motorista'),
+  ('Hanae Barrera','37.668.779-1','(67) 1541-1114',5,'F','Secretaria');
+
+--produto
+
+INSERT INTO produto (nome)
+VALUES
+  ('milho'),
+  ('batata-doce'),
+  ('alface'),
+  ('repolho'),
+  ('abobrinha'),
+  ('Abóbora'),
+  ('pepino'),
+  ('banana'),
+  ('Laranja'),
+  ('melancia'),
+  ('cebola');
+
+-- preço tabelado  
+
+
+--cultura
+
+INSERT INTO cultura (produto,valor,quantidade)
+VALUES
+  (1,'R$9.39',511),
+  (2,'R$7.39',411),
+  (3,'R$26.00',408),
+  (4,'R$4.97',221),
+  (5,'R$45.73',842),
+  (6,'R$32.69',429),
+  (7,'R$22.24',362),
+  (8,'R$28.78',918),
+  (9,'R$19.84',588),
+  (10,'R$15.05',894),
+  (11,'R$5.67',767);
+
+
+--coperado
+
+INSERT INTO cooperado (cpf,nome,celular,cultura,endereco)
+VALUES
+  ('32.276.828-1','Marah Larsen','(86) 7934-0716',8,1),
+  ('36.695.765-0','Pascale Wiggins','(32) 9186-4422',7,2),
+  ('26.547.985-5','Kimberly Farley','(89) 7691-3384',4,3),
+  ('14.953.556-K','Cruz Lancaster','(46) 9179-1587',2,4),
+  ('50.129.972-3','Kane Webster','(91) 3671-2533',9,5),
+  ('35.143.585-2','Omar Levy','(57) 6523-3374',4,6),
+  ('10.618.215-9','Hanae Shannon','(06) 0915-2758',2,7),
+  ('23.248.386-5','Evan Joseph','(59) 8532-5656',8,8),
+  ('23.312.521-0','Kato Salinas','(57) 7746-8227',1,9),
+  ('8.266.453-K','Ann Bowen','(15) 8235-1465',1,10),
+  ('23.165.627-8','Hilel Stevenson','(41) 1540-1276',8,11),
+  ('2.681.966-0','Ryder Chase','(82) 5975-3214',9,12),
+  ('16.695.395-2','Remedios Lopez','(18) 8490-3362',6,13),
+  ('4.750.663-8','Jillian Parsons','(55) 9557-7427',9,14),
+  ('366.036-2','Kenneth Dejesus','(05) 4951-8807',1,15),
+  ('43.295.537-0','Brenden Patton','(16) 5434-1655',9,16),
+  ('46.802.964-2','Linus Fuentes','(38) 8902-7354',7,17),
+  ('28.953.329-K','Adam Randolph','(65) 8642-1403',10,18),
+  ('4.636.210-1','Alvin Alford','(57) 5144-2147',10,19),
+  ('2.778.228-0','Rajah Wolf','(34) 1577-7486',1,20),
+  ('29.804.142-1','Keefe Nichols','(25) 4392-4462',7,21),
+  ('11.213.609-6','Burton Maxwell','(25) 8735-1284',6,22),
+  ('12.107.563-6','Wesley Noel','(17) 3322-3154',5,23),
+  ('30.380.654-7','Oleg Levine','(62) 9813-9656',11,24),
+  ('2.124.072-9','Jeremy Ortiz','(60) 1463-0604',1,25),
+  ('750.520-5','Nita Melendez','(23) 4889-6761',8,26),
+  ('6.564.893-8','Catherine Snider','(51) 4165-9668',4,27),
+  ('364.599-1','Tatiana Stein','(59) 6278-9629',7,28),
+  ('10.579.524-6','Craig Hebert','(15) 0118-8361',5,29),
+  ('28.608.509-1','Odette Brown','(17) 4382-3630',2,30);
+INSERT INTO cooperado (cpf,nome,celular,cultura,endereco)
+VALUES
+  ('27.836.373-2','Jerry Farmer','(41) 4630-4331',5,31),
+  ('8.362.879-0','Ishmael Hays','(04) 8632-2223',9,32),
+  ('22.856.226-2','Jaden Leonard','(08) 4378-6176',11,33),
+  ('11.787.454-0','Hammett Sweet','(04) 5783-3080',2,34),
+  ('5.239.581-K','Jocelyn Boone','(94) 1404-5175',5,35),
+  ('5.403.884-4','Sawyer Avery','(18) 8081-5566',9,36),
+  ('42.859.411-8','Rafael Saunders','(28) 6818-9892',9,37),
+  ('39.651.860-0','Daniel Weiss','(90) 5879-6322',1,38),
+  ('17.527.321-2','Reese Guthrie','(82) 5049-6253',2,39),
+  ('293.562-7','Holmes Mcintosh','(50) 6522-9297',11,40),
+  ('2.617.243-8','Paki Benjamin','(50) 8095-5825',5,41),
+  ('345.514-9','Wilma Cole','(23) 8338-3207',9,42),
+  ('29.978.963-2','Justina Herman','(60) 8275-4288',1,43),
+  ('35.649.837-2','Calvin Key','(53) 6881-4211',3,44),
+  ('32.653.980-5','Harlan Maldonado','(39) 7743-1384',4,45),
+  ('1.548.343-1','Alisa Morrow','(99) 4384-7634',9,46),
+  ('33.599.260-1','Haley Tyson','(94) 4927-6375',9,47),
+  ('8.119.347-9','Abra Sullivan','(40) 2931-4284',6,48),
+  ('30.660.267-5','Malik Lambert','(52) 8171-9332',6,49),
+  ('20.729.835-2','Josephine Young','(43) 1761-4345',2,50);
+INSERT INTO cooperado (cpf,nome,celular,cultura,endereco)
+VALUES
+  ('1.912.492-4','Cody Mcknight','(66) 1953-4468',4,51),
+  ('30.348.236-9','Sara Sears','(74) 4716-8779',9,52),
+  ('14.582.154-1','Lacey Mejia','(31) 8794-1144',4,53),
+  ('4.962.016-0','Alec Colon','(62) 3118-2897',5,54),
+  ('23.595.517-2','Lara Wooten','(09) 3366-2651',2,55),
+  ('44.451.664-K','Rajah Wade','(72) 1338-6747',8,56),
+  ('45.452.486-1','Elijah Chandler','(44) 4567-3741',8,57),
+  ('25.173.529-8','Nash Justice','(43) 2911-1363',1,58),
+  ('24.881.206-0','Fleur Lancaster','(89) 2554-5842',2,59),
+  ('41.744.340-1','Gail Jennings','(17) 3601-8125',3,60),
+  ('2.037.337-7','Germane Stafford','(40) 4811-5283',2,61),
+  ('35.513.184-K','Mohammad Sutton','(24) 8521-4232',11,62),
+  ('16.815.629-4','Dalton Copeland','(01) 1869-8451',11,63),
+  ('2.967.180-K','Pascale Perez','(27) 4684-1788',10,64),
+  ('40.825.952-5','Aquila Melendez','(02) 8442-6304',10,65),
+  ('39.983.936-K','Emi Logan','(72) 7124-6874',6,66),
+  ('44.579.599-2','Sarah O''connor','(34) 9274-6346',6,67),
+  ('47.171.326-0','Whitney Glenn','(77) 1515-7457',5,68),
+  ('40.125.507-9','Virginia Reed','(72) 9221-6472',6,69),
+  ('40.611.188-1','Fredericka Warner','(54) 6768-5023',2,70),
+  ('33.782.793-4','Curran Cervantes','(55) 7925-5548',3,71),
+  ('34.220.346-9','Indigo Bender','(72) 5822-9301',8,72),
+  ('20.936.113-2','Nita Pierce','(87) 7181-5361',9,73),
+  ('39.121.558-8','Quinlan Daniel','(21) 1622-8777',2,74),
+  ('23.156.356-3','Stacey Kane','(47) 4553-8851',1,75),
+  ('34.304.571-9','Odette Mullen','(11) 4151-7527',5,76),
+  ('3.676.278-0','Joy Velez','(40) 7414-8556',11,77),
+  ('1.249.204-9','Ursa Dawson','(03) 4221-6684',4,78),
+  ('37.251.962-2','Jack Russo','(67) 7764-3630',11,79),
+  ('7.586.395-0','Lynn Tillman','(14) 6224-8127',1,80);
+INSERT INTO cooperado (cpf,nome,celular,cultura,endereco)
+VALUES
+  ('33.973.792-4','Fulton Barker','(37) 3273-3177',10,81),
+  ('30.512.417-6','Prescott Farrell','(63) 6435-7993',1,82),
+  ('21.198.339-6','Michael Park','(29) 8692-5613',9,83),
+  ('1.861.979-2','Galvin Lynch','(97) 2444-1676',9,84),
+  ('34.111.650-3','Farrah Mcfadden','(85) 1367-4556',5,85),
+  ('7.786.134-3','Noah Franklin','(48) 5516-8278',1,86),
+  ('45.548.981-4','Robin Strong','(71) 2636-3712',6,87),
+  ('42.942.504-2','Timon Malone','(93) 4806-7661',3,88),
+  ('568.526-5','Quinn Owens','(58) 8365-1155',9,89),
+  ('1.468.214-7','Oleg Boyer','(79) 6858-9387',5,90),
+  ('39.538.901-7','Demetrius Turner','(78) 2351-8148',6,91),
+  ('11.921.297-9','Graham Colon','(81) 7167-9222',1,92),
+  ('213.085-8','Dorian Kelley','(09) 2843-8066',10,93),
+  ('204.859-0','Cassady Mcfadden','(52) 4144-7224',6,94),
+  ('30.131.991-6','Clio Callahan','(44) 7454-2682',7,95),
+  ('38.407.412-K','Linda Hays','(53) 2744-1585',2,96),
+  ('44.774.647-6','Amy Jacobs','(62) 4354-8455',6,97),
+  ('13.600.931-1','Lenore Williams','(12) 5912-2048',10,98),
+  ('228.253-4','Quon George','(54) 0497-4313',5,99),
+  ('32.145.612-K','Aphrodite Tate','(75) 7659-4454',1,100);
